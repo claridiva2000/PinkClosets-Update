385c8222938e329ceb09a779e760f723b3c0fd76
claridiva2000 <claridiva2000@gmail.com>
2019-08-13 02:06:22 -0500

    nav updated

--- pinkcloset/src/components/nav/logo.js
+++ pinkcloset/src/components/nav/logo.js
@@ -3,10 +3,7 @@ import React from 'react';
 const logo = () => {
   return (
     <div style={{ margin: '0px', display: 'flex', justifyContent:'center', borderBottom:'1px solid' }}>
-      <img style={{padding:'0px', margin:'0px'}}
-          src="http://pinkclosets.com/fancyhangerlogo.png"
-          alt="hanger"
-          style={{ width: '80px', padding: '10px' }}/> 
+     
       <div style={{padding:'0'}}>
       <span style={{ color: '#955465', fontSize: '3.5rem', margin: '0', padding:'0px' }}>
           Pink
--- pinkcloset/src/components/nav/navbar.jsx
+++ pinkcloset/src/components/nav/navbar.jsx
@@ -7,14 +7,35 @@ const navbar = () => {
     <div style={{ background: '#EED6DC', borderBottom:'1px solid'}}>
       <nav style={{ listStyle:'none', }}>
         <Logo />
-        <ul style={{display:'flex',listStyle:'none', justifyContent: 'space-between', width: '850px', margin: 'auto', marginTop:'10px', fontSize: '1rem', textAlign: 'center', padding:'0px'}}>
-          <li><NavLink to='/' style={navlinkStyle} activeStyle={activelinkStyle} >Home</NavLink></li>
-          <li><NavLink to='/about' style={navlinkStyle} activeStyle={activelinkStyle}>About</NavLink></li>
-          <li><NavLink to='/services' style={navlinkStyle} activeStyle={activelinkStyle}>Services</NavLink></li>
-          <li><NavLink to='/gallery' style={navlinkStyle} activeStyle={activelinkStyle}>Gallery</NavLink></li>
-          <li><NavLink to='/blog' style={navlinkStyle} activeStyle={activelinkStyle}>Blog</NavLink></li>
-          <li><NavLink to='/contact' style={navlinkStyle} activeStyle={activelinkStyle}>Contact</NavLink></li>
-          <li><NavLink to='/signin' style={navlinkStyle} activeStyle={activelinkStyle}>Admin</NavLink></li>
+        <ul style={{display:'flex',listStyle:'none', justifyContent: 'space-between', width: '1000px', margin: 'auto', marginTop:'10px', fontSize: '1.3rem', textAlign: 'center'}}>
+          <li><NavLink to='/' style={navlinkStyle} activeStyle={activelinkStyle} >Home</NavLink>  <img 
+          src="http://pinkclosets.com/fancyhangerlogo.png"
+          alt="hanger"
+          style={{ width: '35px', height:'25px',  padding: '10px', position: 'relative', top:'10px', left:'5px' }}/> </li> 
+          <li><NavLink to='/about' style={navlinkStyle} activeStyle={activelinkStyle}>About</NavLink><img 
+          src="http://pinkclosets.com/fancyhangerlogo.png"
+          alt="hanger"
+          style={{ width: '35px', height:'25px',  padding: '10px', position: 'relative', top:'10px' }}/></li>
+          <li><NavLink to='/services' style={navlinkStyle} activeStyle={activelinkStyle}>Services</NavLink><img 
+          src="http://pinkclosets.com/fancyhangerlogo.png"
+          alt="hanger"
+          style={{ width: '35px', height:'25px',  padding: '10px', position: 'relative', top:'10px' }}/></li>
+          <li><NavLink to='/gallery' style={navlinkStyle} activeStyle={activelinkStyle}>Gallery</NavLink><img 
+          src="http://pinkclosets.com/fancyhangerlogo.png"
+          alt="hanger"
+          style={{ width: '35px', height:'25px',  padding: '10px', position: 'relative', top:'10px' }}/></li>
+          <li><NavLink to='/blog' style={navlinkStyle} activeStyle={activelinkStyle}>Blog</NavLink><img 
+          src="http://pinkclosets.com/fancyhangerlogo.png"
+          alt="hanger"
+          style={{ width: '35px', height:'25px',  padding: '10px', position: 'relative', top:'10px' }}/></li>
+          <li><NavLink to='/contact' style={navlinkStyle} activeStyle={activelinkStyle}>Contact</NavLink><img 
+          src="http://pinkclosets.com/fancyhangerlogo.png"
+          alt="hanger"
+          style={{ width: '35px', height:'25px',  padding: '10px', position: 'relative', top:'10px' }}/></li>
+          <li><NavLink to='/signin' style={navlinkStyle} activeStyle={activelinkStyle}>Admin</NavLink><img 
+          src="http://pinkclosets.com/fancyhangerlogo.png"
+          alt="hanger"
+          style={{ width: '35px', height:'25px',  padding: '10px', position: 'relative', top:'10px', visibility:'hidden' }}/></li>
         </ul>
        </nav>
     </div>
